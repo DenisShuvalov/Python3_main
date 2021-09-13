9f67d725923b25a131017d3c39d4ac8c4d2af992
DenisShuvalov <dshuvalov@bk.ru>
2021-09-13 16:35:15 +0300

    modify dz1_1 file

--- Lesson_1/Shuvalov_Denis_dz1_3.py
+++ Lesson_1/Shuvalov_Denis_dz1_3.py
@@ -1,14 +1,14 @@
 # procent = int(input('введите число'))
 
-for i in range(100):
-    my_str = str(i + 1)
+for i in range(1, 101):
+    my_str = str(i)
     my_list = list(my_str)
-    if int(my_list[-1]) == 1 and i + 1 != 11:
-        print('{} процент'.format(i + 1))
+    if int(my_list[-1]) == 1 and i != 11:
+        print('{} процент'.format(i))
     elif int(my_list[-1]) > 1 and int(my_list[-1]) <= 4:
-        if i + 1 > 10 and i + 1 <= 14:
-            print('{} процентов'.format(i + 1))
+        if i > 10 and i <= 14:
+            print('{} процентов'.format(i))
         else:
-            print('{} процента'.format(i + 1))
+            print('{} процента'.format(i))
     else:
-        print('{} процентов'.format(i + 1))
+        print('{} процентов'.format(i))
