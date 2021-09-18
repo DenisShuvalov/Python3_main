7bf7ec56c8cff89d4f5b74753e8e71336f2e5cf5
DenisShuvalov <dshuvalov@bk.ru>
2021-09-18 18:45:28 +0300

    modify dz2_4 file

--- Lesson_2/Shuvalov_Denis_dz2_4.py
+++ Lesson_2/Shuvalov_Denis_dz2_4.py
@@ -4,4 +4,4 @@ enter_names = ['инженер-конструктор Игорь', 'главны
 for i in enter_names:
     i = i.title()
     name = i.rpartition(' ')
-    print('Привет ', name[-1], sep=" ")
+    print('Привет,', name[-1]+'!', sep=" ")
