598f211b252cece60ebb5bd49dd0afd7e645db43
DenisShuvalov <dshuvalov@bk.ru>
2021-09-26 21:17:04 +0300

    modify dz5_3 file

--- Lesson_5/Shuvalov_Denis_dz5_3.py
+++ Lesson_5/Shuvalov_Denis_dz5_3.py
@@ -5,6 +5,6 @@ klasses = ['9А', '7В', '9Б', '9В', '8Б', '10А', '10Б', '9А']
 
 result = ((t, k) for t, k in zip_longest(tutors, klasses))
 
-print(type(result), *result, sep=' ')
-
-print(next(result))
+print(*result, sep=' ')
+print(type(result))
+# print(next(result))
