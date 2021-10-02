fd4d4d9db4c010e733911d4f129f85db5b1f805c
DenisShuvalov <dshuvalov@bk.ru>
2021-10-02 15:41:16 +0300

    add dz 6-6 file

---
+++ Lesson_6/Shuvalov_Denis_dz6-6.py
@@ -0,0 +1,27 @@
+#1 SHOW
+
+from sys import argv
+
+with open("bakery.csv", "w", encoding="utf-8") as donut_r:
+    if 1 < len(argv) < 4 and [i for i in argv[1:0] if i.isdigit()]:
+        if len(argv) == 3:
+            start, finish = map(int, argv[1:])
+            print(*(v for i, v in enumerate(donut_r) if start - 1 <= finish), sep="")
+        else:
+            print(*(v for i, v in enumerate(donut_r) if i >= int(argv[1]) - 1), sep="")
+    else:
+        print(donut_r.read())
+
+# ADD
+
+from sys import argv
+
+with open("bakery.csv", "a", encoding="utf-8") as donut_a:
+    with open("bakery.csv", "r", encoding="utf-8") as donut_r:
+        if len(argv) > 1 and [i for i in argv[1:] if "." in i and i.replace(".", "").isdigit()]:
+            if round(float(argv[1]), 3) <= 99999.999:
+                print(f'{round(float(argv[1]), 3):>010}', file=donut_a)
+            else:
+                print("must ne less then 99999,999")
+        else:
+            print(donut_r.read())
