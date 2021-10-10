48bf692cbc7a45629c859d5944db4c146cfc0da5
DenisShuvalov <dshuvalov@bk.ru>
2021-10-10 17:15:10 +0300

    Modify dz 9-2 file

--- Lesson9/Shuvalov_Denis_dz9-2.py
+++ Lesson9/Shuvalov_Denis_dz9-2.py
@@ -1,12 +1,25 @@
 class Road:
-    def __init__(self, length, width):
-        self._length = length
-        self._width = width
+    def __init__(self, le, wi):
+        self._length = le
+        self._width = wi
 
-    def get_full_profit(self, weight=40, thickness=25):
-        return f"{self._length} м * {self._width} м * {weight} кг * {thickness} см =" \
-               f" {(self._length * self._width * weight * thickness) / 1000} т"
+    def calc(self, weight=40, thickness=25):
+        print(f"масса асфальта - {self._length * self._width * weight * thickness/ 1000} т")
 
 
-road_1 = Road(5000, 20)
-print(road_1.get_full_profit())
+        # return f"{self._length} м * {self._width} м * {weight} кг * {thickness} см =" \
+        #        f" {(self._length * self._width * weight * thickness) / 1000} т"
+
+
+def main():
+    while True:
+        try:
+            road_1 = Road(int(input("Enter width(m): ")), int(input("Enter length(m): ")))
+            road_1.calc()
+            break
+        except ValueError:
+            print("Int")
+
+
+# road_1 = Road(5000, 20)
+# print(road_1.get_full_profit())
